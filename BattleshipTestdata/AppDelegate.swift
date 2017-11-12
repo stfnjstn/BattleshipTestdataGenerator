489d8a3603c30599d7dd9ad866a8ee5aa5271c13
stfnjstn <stfn.jstn@gmail.com>
2017-11-12 15:15:42 +0100

    Initial Commit

---
+++ BattleshipTestdata/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BattleshipTestdata
+//
+//  Created by STEFAN JOSTEN on 12.11.17.
+//  Copyright © 2017 Stefan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BattleshipTestdata/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BattleshipTestdata
+//
+//  Created by STEFAN JOSTEN on 12.11.17.
+//  Copyright © 2017 Stefan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
